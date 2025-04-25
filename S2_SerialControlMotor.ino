3dc51089476cbf9fcac396e31ca8fb69e5074c05
Darren Chan <xuanzz@msn.com>
2025-04-25 13:27:45 +0800

    change pH data format

--- S2_SerialControlMotor.ino
+++ S2_SerialControlMotor.ino
@@ -125,7 +125,7 @@ void GetpHValue() // function for getting temperature
  
   // PH_step (Voltage/pH Unit) = (Voltage@PH7-Voltage@PH4)/(PH7 - PH4)
   float pH = 7+((2.5 - voltage)/0.1841); // PH_probe = PH7-((Voltage@PH7-Voltage@probe)/PH_step)
-  Serial2.print("P="); //Print word pH in Serial Monitor
+  Serial2.print("P,"); //Print word pH in Serial Monitor
   Serial2.println(pH); //Print pH value in Serial Monitor  
 }
 
